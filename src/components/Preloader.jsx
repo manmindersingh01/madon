7daf62dcfbd9b19893d2095f79075f48f2e17206
manminder singh <bewithme2407@gmail.com>
2024-08-18 18:52:31 +0530

    new

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -6,11 +6,11 @@ const Preloader = () => {
   })
   return (
     <div className='preloader h-screen w-full bg-zinc-950 text-white font-moli flex justify-center items-center overflow-hidden'>
-      <div className='texts-container flex gap-6 justify-center items-center overflow-hidden'>
-        <span className='font-bold text-9xl tracking-wide'>lets</span>
-        <span className='font-bold tracking-wide text-9xl  text-yellow-300'>style</span>
-        <span className='font-bold tracking-wide text-9xl '>with</span>
-        <span className='font-extrabold text-red-500 text-9xl tracking-wide'>MADON</span>
+      <div className='texts-container flex gap-2 md:gap-6 justify-center items-center overflow-hidden'>
+        <span className='font-bold md:text-9xl tracking-wide'>lets</span>
+        <span className='font-bold tracking-wide md:text-9xl  text-yellow-300'>style</span>
+        <span className='font-bold tracking-wide md:text-9xl '>with</span>
+        <span className='font-extrabold text-2xl text-red-500 md:text-9xl tracking-wide'>MADON</span>
       </div>
 
     </div>
